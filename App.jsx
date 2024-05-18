592e6aad3f6b4e00a071795b09d9d78acf10a9de
DHAMODHARAN S <148353944+dhamodharanECE@users.noreply.github.com>
2024-05-18 08:11:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import App1 from './components/App1'
+import App2 from './components/App2'
+import App3 from './components/App3'
+import App4 from './components/App4'
+import App6 from './components/App6'
+import App7 from './components/App7'
+import App51 from './components/App51'
+
+
+
+
+function App() {
+  return (
+<>
+<App1 />
+<App2 />
+<App3/>
+<App4/>
+<App51/>
+<App6/>
+<App7/>
+
+</>
+  )
+}
+
+export default App
